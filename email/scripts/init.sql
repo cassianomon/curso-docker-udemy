fe71de42571d39c4003683eb7d27c29ff58952b8
Cassiano Montanari <cassianomontanari@gmail.com>
2017-10-03 20:27:14 -0300

    Update do projeto no GitHub

--- email/scripts/init.sql
+++ email/scripts/init.sql
@@ -3,7 +3,7 @@ create database email_sender;
 \c email_sender
 
 create table emails (
-    id serial not null
+    id serial not null,
     data timestamp not null default current_timestamp,
     assunto varchar(100) not null,
     mensagem varchar(250) not null
